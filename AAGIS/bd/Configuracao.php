0d4bf5a57589cf8a04e43371baa3edfab947ae4a
Mateus Bino Belode <mateusbinobelode@gmail.com>
2025-11-06 15:42:30 -0300

    ceryo

--- AAGIS/bd/Configuracao.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //configurações básicas do bd e puxar o bd
-    define("HOST","localhost");
-    define("USUARIO","root");   
-    define("SENHA","");
-    define("BANCO","estagios");
-?>
---
+++ bd/Configuracao.php
@@ -0,0 +1,7 @@
+<?php
+    //configurações básicas do bd e puxar o bd
+    define("HOST","localhost");
+    define("USUARIO","root");   
+    define("SENHA","");
+    define("BANCO","u157320114_extensaog3g4");
+?>
